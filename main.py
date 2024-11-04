5b6fb77f864896a0482cadcedd72b5a9db385971
Ed Scrimaglia <edgardo.scrimaglia@gmail.com>
2024-11-04 19:32:17 -0300

    oauth2 coded

--- main.py
+++ main.py
@@ -1,28 +1,52 @@
 
-from datetime import datetime
+import datetime
 import uuid
-from pydantic import BaseModel
-from fastapi import FastAPI, HTTPException
+from pydantic import AfterValidator, BaseModel
+from fastapi import FastAPI, HTTPException, Depends, status
 from sqlmodel import Field, SQLModel, create_engine, Session, select
 import rabbitmq as rb
+from oauth2 import User, DataBase, Token, Autenticator
 import json
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+import jwt
+import pytz
+from typing import TypeVar, Annotated
+from custom_validation import ValidateListToStr
+from enum import Enum
 
+local_timezone = pytz.timezone('America/Argentina/Buenos_Aires')
+# Annotated for ListToStr objects
+T = TypeVar('T')
+ValidList = Annotated[list[T], AfterValidator(ValidateListToStr.convert_list_to_str)]
+
+class Estado(Enum):
+    Confirmado = "CNF"
+    Pendiente = "PND"
+    Cancelado = "CAN"
 class Producto(BaseModel):
     producto: str = Field(default=uuid.uuid4())
-    cantidad: int
-
+    cantidad: float = Field(..., gt=0)
+class ProductoBase(BaseModel):
+    producto: ValidList[Producto]
+    estado: Estado | None = "CNF" 
+    total: float | None = None
 class PedidoBase(SQLModel):
     producto: str
-    estado: str = Field(default="CNF") 
-    creacion: datetime
+    estado: Estado | None = "CNF" 
     total: float | None = None
-
 class Pedido(PedidoBase, table=True):
     id: str = Field(default_factory=lambda: str(uuid.uuid4()), primary_key=True)
     userid: str = Field(default_factory=lambda: str(uuid.uuid4()))
-
+    costo: float = Field(default=12.6)
+    creacion: datetime.datetime = Field(default_factory=lambda: datetime.datetime.now(local_timezone))
+class PedidoPrecio(PedidoBase):
+    userid: str = Field(default_factory=lambda: str(uuid.uuid4())) 
+    costo: float = Field(default=12.6)
 
 app = FastAPI(title="IAEW", description="REST Full API TP - Grupo 1 - 2024", version="1.0.0")
+
+# Dependencia para el esquema de autenticación
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 sqlite_file_name = "iaew.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -30,7 +54,7 @@ engine = create_engine(sqlite_url, echo=True)
 SQLModel.metadata.create_all(engine)
 
 @app.post("/api/v1/pedido")
-def create_pedido(pedido: PedidoBase):
+def create_pedido(pedido: ProductoBase):
     with Session(engine) as session:
         db_pedido = Pedido.model_validate(pedido)
         session.add(db_pedido)
@@ -53,16 +77,75 @@ def publish_pedido(body: str):
     
 @app.get("/api/v1/pedidos")
 def read_pedidos():
+    list_pedidos = []
     with Session(engine) as session:
-        pedidos = session.exec(select(Pedido)).all()
-        return pedidos
+        reistros_pedidos = session.exec(select(Pedido)).all()
+        for reg in reistros_pedidos:
+            list_pedidos.append({"pedidoId": reg.id, "userId":reg.userid, "producto":reg.producto, "creacion":reg.creacion, "total":reg.total})
+        return list_pedidos
 
 @app.get("/api/v1/pedidos/{id}")
 async def pedidos_by_id(id: str):
     with Session(engine) as session:
         pedidos = session.exec(select(Pedido)).all()
     
-    for pedido in pedidos:
-        if pedido.id == id:
-            return pedido
+    for reg in pedidos:
+        if reg.id == id:
+            return {"pedidoId": reg.id, "userId":reg.userid, "producto":reg.producto, "creacion":reg.creacion, "total":reg.total}
     raise HTTPException(status_code=404, detail="El pedido no existe")
+
+@app.post("/api/v1/token", response_model=Token, include_in_schema=False)
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = Autenticator.authentication(DataBase.users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = datetime.timedelta(minutes=Autenticator.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = Autenticator.create_access_token({"sub": user['username']}, local_timezone, access_token_expires)
+    return {"access_token": access_token, "token_type": "Bearer"}
+
+@app.get("/api/v1/costo", include_in_schema=False)
+async def read_costo_pedidos(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    
+    try:
+        payload = jwt.decode(token, Autenticator.SECRET_KEY, algorithms=[Autenticator.ALGORITHM])
+        username: str = payload.get("sub")
+        user = Autenticator.authentication(DataBase.users_db, username)
+        if not username:
+            raise credentials_exception
+        token_data = User(username=username)
+        expirated_token = Autenticator.validate_expiration(payload)
+        if expirated_token:
+           raise credentials_exception
+    except jwt.PyJWTError as error:
+        credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail=str(error),
+        headers={"WWW-Authenticate": "Bearer"},
+        )
+        raise credentials_exception
+    except jwt.ExpiredSignatureError as error:
+        credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail=str(error),
+        headers={"WWW-Authenticate": "Bearer"},
+        )
+        raise credentials_exception
+    user = DataBase.users_db.get(token_data.username)
+    
+    if not user:
+        raise credentials_exception
+    list_pedidos = []
+    with Session(engine) as session:
+        reistros_pedidos = session.exec(select(Pedido)).all()
+        for reg in reistros_pedidos:
+            list_pedidos.append({"pedidoId": reg.id, "userId":reg.userid, "producto":reg.producto, "creacion":reg.creacion, "total":reg.total, "costo": reg.costo})
+    return list_pedidos
---
+++ oauth2.py
@@ -0,0 +1,72 @@
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
+import jwt
+import datetime
+from datetime import timezone
+
+# Modelo de usuario y token
+class User(BaseModel):
+    username: str
+    full_name: str | None = None
+    email: str | None = None
+    disabled: bool | None = None
+
+class UserInDB(User):
+    hashed_password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class DataBase():
+    users_db = {
+        "edscrimaglia": {
+            "username": "edscrimaglia",
+            "full_name": "Edgardo Scrimaglia",
+            "email": "edscrimaglia@octupus.com",
+            "hashed_password": "Iaew-2024$",
+            "disabled": False,
+        }
+    }
+
+    def __str__(self) -> str:
+        return self.users_db 
+
+import datetime
+import jwt 
+
+class Autenticator:
+    SECRET_KEY = "mi_clave_secreta"
+    ALGORITHM = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+    @classmethod
+    def authentication(cls,db, username: str, password: str | None = None):
+        user = db.get(username)
+        if not username in db or not password in [user['hashed_password'],None]:
+            return False
+        return user 
+
+    @classmethod
+    def create_access_token(cls,data: dict, timezone, expires_delta: datetime.timedelta = 3600):
+        to_encode = data.copy()
+        expire = datetime.datetime.now(timezone) + expires_delta
+        to_encode.update({"exp": expire})
+        encoded_jwt = jwt.encode(to_encode, Autenticator.SECRET_KEY, algorithm=Autenticator.ALGORITHM)
+        return encoded_jwt
+
+    @classmethod
+    def validate_expiration(cls, payload: dict):
+        expiration_timestamp = payload.get('exp')
+        
+        if expiration_timestamp is None:
+            raise ValueError("Expiration timestamp 'exp' is missing from payload")
+        
+        #exp_time = datetime.datetime.fromtimestamp(expiration_timestamp, tz=timezone)
+        exp_time = exp_datetime = datetime.datetime.fromtimestamp(expiration_timestamp, datetime.timezone.utc)
+        #now_time_zone = datetime.datetime.now().replace(tzinfo=timezone)
+        now_time = datetime.datetime.now(datetime.timezone.utc)
+        if exp_datetime <= now_time:
+            return True
+        return False
